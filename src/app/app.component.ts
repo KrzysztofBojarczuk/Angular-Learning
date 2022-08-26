93713a5a21ea659d9a7aa07b4a92c69a9f13ad8e
Krzysztof <krzysztofbojarczuk@wp.pl>
2022-08-26 12:34:56 +0200

    List component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
+import { compileNgModule } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'AngualrLearning';
+  name: string = '';
+  /**
+   *
+   *
+   */
+  constructor() {}
+
+  handleClick(value: any) {
+    console.log(value);
+  }
 }
