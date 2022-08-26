14e406987f927d46248f4c1eaa069af12b48ebb6
Krzysztof <krzysztofbojarczuk@wp.pl>
2022-08-26 14:18:04 +0200

    ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { compileNgModule } from '@angular/compiler';
 import { Component } from '@angular/core';
 
+interface Pokemon {
+  id: number;
+  name: string;
+  type: string;
+  isCool: boolean;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,6 +18,27 @@ export class AppComponent {
    *
    *
    */
+
+  pokemons: Pokemon[] = [
+    {
+      id: 1,
+      name: 'Mike',
+      type: 'electric',
+      isCool: false,
+    },
+    {
+      id: 2,
+      name: 'Frank',
+      type: 'fire',
+      isCool: false,
+    },
+    {
+      id: 3,
+      name: 'Garfield',
+      type: 'garfield',
+      isCool: true,
+    },
+  ];
   constructor() {}
 
   handleClick(value: any) {
