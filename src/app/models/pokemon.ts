2b51967ac05ff30e3652d351dcee2c4536db8926
Krzysztof <krzysztofbojarczuk@wp.pl>
2022-08-30 09:55:26 +0200

    Checkboxes (Template Driven Forms)
    Checkboxes (Template Driven Forms)

--- src/app/models/pokemon.ts
+++ src/app/models/pokemon.ts
@@ -1,7 +1,8 @@
 export interface Pokemon {
-    id: number;
-    name: string;
-    type: string;
-    isCool: boolean;
-    isStylisz: boolean;
-  }
+  id: number;
+  name: string;
+  type: string;
+  isCool: boolean;
+  isStylisz: boolean;
+  acceptTerms: boolean;
+}
